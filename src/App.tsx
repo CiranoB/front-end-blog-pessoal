9e5601c5e10b7d2313de7d6df35c8745cadd8f6e
Cirano <ciranobelardony@gmail.com>
2022-07-03 23:31:00 -0300

    Atividade 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './componentes/estaticos/footer/Footer';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Home from './componentes/paginas/home/Home';
+import Login from './componentes/paginas/login/Login';
+// import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home/>
-      <Footer />
-    </>
+    <Router>
+    <Navbar />
 
+    <div style={{ minHeight: '100vh' }}>
+       <Routes> 
+
+        <Route
+         path="/home" 
+         element={<Home />}
+        />
+
+        <Route
+         path="/" 
+         element={< Login />} 
+        />
+
+        <Route 
+         path="/login" 
+         element={< Login />} 
+        />
+{/* 
+        <Route 
+         path="/cadastrousuario" 
+         element={< CadastroUsuario />} 
+        />    */}
+
+      </Routes>
+    </div>
+
+    <Footer />
+  </Router>
   );
 }
 
