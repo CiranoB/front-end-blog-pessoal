350721f6434e75f2385bc414fb733c16c51d8dfd
Cirano <ciranobelardony@gmail.com>
2022-07-12 22:54:41 -0300

    Post, Put, Delete, Redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,88 @@ import Navbar from './componentes/estaticos/navbar/Navbar';
 import Home from './componentes/paginas/home/Home';
 import Login from './componentes/paginas/login/Login';
 import CadastroUsuario from './componentes/paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './componentes/temas/listatema/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './componentes/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
+    
+    <Provider store={store}>
+      
     <Router>
-    <Navbar />
+      <Navbar />
 
-    <div style={{ minHeight: '100vh' }}>
-       <Routes> 
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
 
-        <Route
-         path="/home" 
-         element={<Home />}
-        />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
 
-        <Route
-         path="/" 
-         element={< Login />} 
-        />
+          <Route
+            path="/"
+            element={< Login />}
+          />
 
-        <Route 
-         path="/login" 
-         element={< Login />} 
-        />
+          <Route
+            path="/login"
+            element={< Login />}
+          />
 
-        <Route 
-         path="/cadastrousuario" 
-         element={< CadastroUsuario />} 
-        />   
+          <Route
+            path="/cadastrousuario"
+            element={< CadastroUsuario />}
+          />
 
-      </Routes>
-    </div>
+          <Route
+            path="/temas"
+            element={< ListaTema />}
+          />
 
-    <Footer />
-  </Router>
+          <Route
+            path="/posts"
+            element={< ListaPostagem />}
+          />
+
+          <Route 
+          path="/formularioPostagem" 
+          element={<CadastroPost />} />
+
+          <Route 
+          path="/formularioPostagem/:id" 
+          element={<CadastroPost />} />
+
+          <Route 
+          path="/formularioTema" 
+          element={<CadastroTema />} />
+
+          <Route 
+          path="/formularioTema/:id" 
+          element={<CadastroTema />} />
+
+          <Route 
+          path="/deletarPostagem/:id" 
+          element={<DeletarPostagem />} />
+
+          <Route 
+          path="/deletarTema/:id" 
+          element={<DeletarTema />} />
+
+
+
+        </Routes>
+      </div>
+
+      <Footer />
+    </Router>
+    </Provider>
   );
 }
 
--- src/componentes/paginas/home/Home.tsx
+++ src/componentes/paginas/home/Home.tsx
@@ -1,74 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Grid, Button } from '@material-ui/core';
 import { Box } from '@mui/material'
 import './Home.css';
+import TabPostagem from '../../postagens/tabpostagem/TabPostagem';
+import useLocalStorage from 'react-use-localstorage';
+import { useNavigate } from 'react-router-dom';
+import ModalPostagem from '../../postagens/modalPostagem/ModalPostagem';
 
 
 function Home() {
+
+    let history = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          history("/login")
+  
+      }
+  }, [token])
     return (
         <>
-            {/* Display da página */}
-            <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-                className='caixa'
-            >
-
-                {/* Primeiro Grid */}
-
-                <Grid alignItems="center" item xs={12} sm={6} >
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-
-                        <Typography
-                            variant="h3"
-                            gutterBottom
-                            color="textPrimary"
-                            component="h3"
-                            align="center"
-                            className='titulo'
-                        >Seja bem vindo(a)!
-                        </Typography>
-
-                        <Typography
-                            variant="h5"
-                            gutterBottom
-                            color="textPrimary"
-                            component="h5"
-                            align="center"
-                            className='titulo'
-                        >Expresse aqui os seus pensamentos e opiniões!
-                        </Typography>
-
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
-
-                    <Box  display="flex" justifyContent="center">
+                    <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
-
-                        <Button className='verpost'
-                            variant="outlined"
-                        >Ver Postagens
-                        </Button>
-                        <p>                         </p>
-
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
-
-                { /* Segundo Grid */}
-
-                <Grid item xs={12} sm={6} >
-                    <Typography align="center">
-                    <img src="https://i.imgur.com/zMfC6Ip_d.webp?maxwidth=760&fidelity=grand" alt="" width="500px" height="500px" />
-                    </Typography>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
-
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
-
             </Grid>
-
         </>
     );
 }
